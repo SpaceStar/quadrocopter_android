505ce7df3946221266ffbae5e1306b82f21f15eb
SpaceStar <spacestar.ru@gmail.com>
2017-08-16 00:06:35 +0600

    Automatically wifi connecting

--- app/src/main/java/a383/quadrocopter/MainActivity.java
+++ app/src/main/java/a383/quadrocopter/MainActivity.java
@@ -1,5 +1,8 @@
 package a383.quadrocopter;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -49,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             actionBar.hide();
         }
         initViews();
+        initWifi();
 
         state = new byte[STATE_SIZE];
 
@@ -183,6 +187,21 @@ public class MainActivity extends AppCompatActivity {
         rrButton.setOnTouchListener(onTouchListener);
         ruButton.setOnTouchListener(onTouchListener);
         rdButton.setOnTouchListener(onTouchListener);
+    }
+
+    private void initWifi() {
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = "\"" + getApplicationContext().getResources().getString(R.string.wifi_ssid) + "\"";
+        conf.preSharedKey = "\"" + getApplicationContext().getResources().getString(R.string.wifi_pass) + "\"";
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled())
+            wifiManager.setWifiEnabled(true);
+        int networkId = wifiManager.addNetwork(conf);
+
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(networkId, true);
+        wifiManager.reconnect();
     }
 
     @Override

d86ac3ff60be728b5647ef2da69b9c44fe293256
Alexander Marushka <marushkaalex@gmail.com>
2017-12-17 23:42:59 +0300

    config to Consts

---
+++ app/src/main/java/com/spacestar/quadrocopter/Consts.java
@@ -0,0 +1,18 @@
+package com.spacestar.quadrocopter;
+
+public class Consts {
+    public static final String SERVER_IP = "192.168.1.1";
+    public static final int SERVER_PORT = 8888;
+
+    public static final long UDP_PERIOD = 50;
+    public static final long WIFI_PERIOD = 500;
+    public static final int STATE_SIZE = 5;
+
+    public static final double ADC_CONST = 0.01278;
+    public static final double MAX_VOLTAGE = 12.9;
+    public static final double MIN_VOLTAGE = 9;
+    public static final double SMALL_VOLTAGE = 9.9;
+
+    private Consts() {
+    }
+}
--- app/src/main/java/com/spacestar/quadrocopter/MainActivity.java
+++ app/src/main/java/com/spacestar/quadrocopter/MainActivity.java
@@ -17,22 +17,12 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import static com.spacestar.quadrocopter.Consts.*;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String SERVER_IP = "192.168.1.1";
-    public static final int SERVER_PORT = 8888;
-
-    public static final long UDP_PERIOD = 50;
-    public static final long WIFI_PERIOD = 500;
-    public static final int STATE_SIZE = 5;
-
-    public static final double ADC_CONST = 0.01278;
-    public static final double MAX_VOLTAGE = 12.9;
-    public static final double MIN_VOLTAGE = 9;
-    public static final double SMALL_VOLTAGE = 9.9;
-
     private boolean mWifiDisable;
 
     protected TextView mWifiStateText;
